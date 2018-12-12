71b3d725495fb2af62271709213238f0accaec6a
Victor Sebastian Mølschau Lauridsen <vict4694@edu.eal.dk>
2018-12-12 09:11:05 +0100

    Add project files.

---
+++ Ex36-Observer Pattern 2/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex36_Observer_Pattern_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
